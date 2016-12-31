da4749583767e6d9ff82fff75a2bd166fbc0ea43
Henry <zhy289044312@gmail.com>
2016-12-31 22:21:00 +0800

    [add] 增加单元测试文件

---
+++ ZHYCodingModelTestCase.m
@@ -0,0 +1,39 @@
+//
+//  ZHYCodingModelTestCase.m
+//  ZHYCodingModel
+//
+//  Created by Henry on 2016/12/31.
+//  Copyright © 2016年 John Henry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZHYCodingModelTestCase : XCTestCase
+
+@end
+
+@implementation ZHYCodingModelTestCase
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
