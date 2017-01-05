67b8867c2d61030cc74cef4132ece1835d36b33f
Henry <zhy289044312@gmail.com>
2017-01-06 01:00:22 +0800

    [remove] 移除main中无效的代码，使用单元测试文件

--- ZHYCodingModel/main.m
+++ ZHYCodingModel/main.m
@@ -6,31 +6,8 @@
 //  Copyright © 2016年 John Henry. All rights reserved.
 //
 
-#import "TestClass.h"
-#import "TestSubClass.h"
 #import <Cocoa/Cocoa.h>
-#import <objc/objc-runtime.h>
 
 int main(int argc, const char * argv[]) {
-    TestSubClass *archiverObject = [[TestSubClass alloc] init];
-
-    NSMutableString *path = [NSMutableString stringWithFormat:@"%@/test.dat", NSHomeDirectory()];
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-
-    BOOL existed = [fileManager fileExistsAtPath:path];
-    if (existed) {
-        NSError *error;
-        BOOL res = [fileManager removeItemAtPath:path error:&error];
-        if (!res) {
-            NSLog(@"Error: %@", error);
-        }
-    }
-
-    BOOL archiverSucceed = [NSKeyedArchiver archiveRootObject:archiverObject toFile:path];
-    NSLog(@"Test encode: %@\n", (archiverSucceed ? @"YES" : @"NO"));
-
-    id unarchiverObject = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
-    NSLog(@"Test decode: %@\n", unarchiverObject);
-
     return NSApplicationMain(argc, argv);
 }
