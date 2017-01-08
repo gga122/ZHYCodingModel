dbdec0602c4923de3ecaed7d349a0caae63ed188
Henry <zhy289044312@gmail.com>
2017-01-09 00:59:09 +0800

    [add] 增加用于测试方法重写不匹配的类

---
+++ ZHYCodingModel/ZHYNotPairTestClass.h
@@ -0,0 +1,20 @@
+//
+//  ZHYNotPairTestClass.h
+//  ZHYCodingModel
+//
+//  Created by Henry on 2017/1/9.
+//  Copyright © 2017年 John Henry. All rights reserved.
+//
+
+#import "ZHYTestClass.h"
+
+/**
+ *  This class is designed to test overridden methods with coding.
+ *
+ *  I overridden the methods declared in super class but not coupled.
+ *
+ */
+
+@interface ZHYNotPairTestClass : ZHYTestClass
+
+@end
---
+++ ZHYCodingModel/ZHYNotPairTestClass.m
@@ -0,0 +1,13 @@
+//
+//  ZHYNotPairTestClass.m
+//  ZHYCodingModel
+//
+//  Created by Henry on 2017/1/9.
+//  Copyright © 2017年 John Henry. All rights reserved.
+//
+
+#import "ZHYNotPairTestClass.h"
+
+@implementation ZHYNotPairTestClass
+
+@end
