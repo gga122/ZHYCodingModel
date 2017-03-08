8b16b9f6cd4d2ad08fb771a6660917596ae3a3f7
MickyZhu <mickyzhu@tencent.com>
2017-03-08 20:06:47 +0800

    [add] 增加支持系统结构体的测试类

---
+++ ZHYSystemStructTestClass.h
@@ -0,0 +1,17 @@
+//
+//  ZHYSystemStructTestClass.h
+//  ZHYCodingModel
+//
+//  Created by MickyZhu on 8/3/2017.
+//  Copyright © 2017 John Henry. All rights reserved.
+//
+
+#import "ZHYCodingModel.h"
+
+/**
+ *  This class is designed to test system struct coding. like CGRect, CGPoint, CGSize, etc
+ *
+ */
+@interface ZHYSystemStructTestClass : ZHYCodingModel
+
+@end
---
+++ ZHYSystemStructTestClass.m
@@ -0,0 +1,13 @@
+//
+//  ZHYSystemStructTestClass.m
+//  ZHYCodingModel
+//
+//  Created by MickyZhu on 8/3/2017.
+//  Copyright © 2017 John Henry. All rights reserved.
+//
+
+#import "ZHYSystemStructTestClass.h"
+
+@implementation ZHYSystemStructTestClass
+
+@end
