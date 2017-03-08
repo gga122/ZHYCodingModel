0a6f769551e1cf73ce9cf636d257b8a31c04c23c
MickyZhu <mickyzhu@tencent.com>
2017-03-08 20:14:50 +0800

    [improve] 补充相等逻辑

--- ZHYSystemStructTestClass.m
+++ ZHYSystemStructTestClass.m
@@ -10,4 +10,39 @@
 
 @implementation ZHYSystemStructTestClass
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _rect = CGRectMake(1, 2, 3, 4);
+        _point = CGPointMake(8, 26);
+        _size = CGSizeMake(7, 25);
+    }
+    
+    return self;
+}
+
+- (BOOL)isEqual:(id)object {
+    if (![object isKindOfClass:[ZHYSystemStructTestClass class]]) {
+        return NO;
+    }
+    
+    return [self isEqualToZHYSystemStructTestClass:object];
+}
+
+- (BOOL)isEqualToZHYSystemStructTestClass:(ZHYSystemStructTestClass *)object {
+    if (!CGRectEqualToRect(object->_rect, _rect)) {
+        return NO;
+    }
+    
+    if (!CGPointEqualToPoint(object->_point, _point)) {
+        return NO;
+    }
+    
+    if (!CGSizeEqualToSize(object->_size, _size)) {
+        return NO;
+    }
+    
+    return YES;
+}
+
 @end
